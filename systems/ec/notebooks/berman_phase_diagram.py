7e77683bc4f6845ebf4a23b6f2597bae8f3cc1af
Mitchell McMillan <mitchellmcm27@gmail.com>
2022-11-01 14:35:44 +0000

    berman endmembers

--- systems/ec/notebooks/berman_phase_diagram.py
+++ systems/ec/notebooks/berman_phase_diagram.py
@@ -11,6 +11,11 @@ import contextlib
 import datetime as dt
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy import interpolate
+import math
+import matplotlib.colors as mcolors
+
+from boustrophedon import boustrophedon
 
 def supress_stdout(func):
     def wrapper(*a, **ka):
@@ -20,14 +25,19 @@ def supress_stdout(func):
     return wrapper
 
 @supress_stdout
-def update_system(sys,T,P):
+def update_system(sys,T,P, comp=None, phase_library=None):
+    if sys is None:
+        assert(comp is not None)
+        assert(phase_library is not None)
+        sys = System(
+            T=800,
+            P=0.25*units.GPA,
+            comp=comp,
+            options={'grid_spacing':0.2},
+            phase_library=phase_library,
+        )
     sys.update(T=T, P=P)
-    amts = sys.stable_assemblage.amounts[sys.stable_assemblage.amounts>0.01]
-    names = sys.stable_assemblage.names[sys.stable_assemblage.amounts>0.01]
-    names_sorted = [x for _, x in sorted(zip(amts, names), reverse=True)]
-    amts_sorted = sorted(amts, reverse=True)
-    hash = " ".join(names_sorted)
-    return hash
+    return sys
 
 def run(phase_symbols = None, oxides = None, T=None, P=None, name=None):
 
@@ -35,7 +45,7 @@ def run(phase_symbols = None, oxides = None, T=None, P=None, name=None):
         "Qz", "Coe",
         "Grt",  
         'Di','Jd','cEn', #'Cpx'
-        'En', 'Fs', 'En',
+        'En', 'Fs',
         "Ky",
         "Ab", "An",
         "Rt",
@@ -59,52 +69,84 @@ def run(phase_symbols = None, oxides = None, T=None, P=None, name=None):
     phases = db.get_phases(phase_symbols_berman)
     phase_library = PhaseLibrary(phases)
 
-    system = System(
-        T=800,
-        P=0.25*units.GPA,
-        comp=oxide_comp,
-        options={'grid_spacing':0.2},
-        phase_library=phase_library
-    )
+    system = update_system(
+            None,
+            T=800,
+            P=0.25*units.GPA,
+            comp=oxide_comp,
+            phase_library=phase_library,
+        )
 
     Ts, Ps = np.meshgrid(T,P)
+
     comps = np.empty(Ts.shape, dtype='U32')
+    energy = np.empty(Ts.shape, dtype='float')
+    assemblages = np.empty(Ts.shape, dtype='object')
+    
     for j,t in enumerate(T):
         for i,p in enumerate(P):
-            comps[i][j] = update_system(system, t,p)
+            update_system(system, t,p)
+            names = system.stable_assemblage.names[system.stable_assemblage.amounts>0.001]
+            hash = " ".join(sorted(names))
+            comps[i][j] = hash
+            energy[i][j] = system.stable_assemblage.total_energy
+            assemblages[i][j] = system.stable_assemblage
 
     try:
-        categories = np.unique(comps).flatten()
-        cmap = plt.get_cmap('tab20')
-        colormap = cmap(np.linspace(0, 1, 20))
-        colorLookup = dict([(key, value) for key, value in zip(categories, colormap)])
-
-        colors = [colorLookup[c] for c in comps.flatten()]
-
-        shape = (comps.shape[0], comps.shape[1], 4)
-        colorgrid = np.reshape(colors, shape)
-
-        fig =plt.figure(figsize=(8,10),facecolor='white')
-        ax  =plt.subplot(211)
-        for q,cat in enumerate(categories):
-            indx = comps.flatten() == cat
-            ax.scatter(Ts.flatten()[indx], Ps.flatten()[indx], label=cat, color=colorLookup[cat])
-        plt.xlim([Tmin,Tmax])
-        plt.ylim([Pmin,Pmax])
-        plt.ylabel("Pressure (Pa)")
-
-        leg1 = ax.legend()
-
-        ax2 = plt.subplot(212)
-        ax2.imshow(colorgrid, origin='lower', extent=(Tmin,Tmax,Pmin/units.GPA,Pmax/units.GPA), aspect="auto")
-        ax2.legend(*ax.get_legend_handles_labels())
-        plt.ylabel("Pressure (GPa)")
-        plt.xlabel("Temperature (K)")
-        plt.title(name)
-        plt.savefig(name + "_"+ dt.datetime.now().isoformat() + ".png")
+        plot_result(Ts,Ps,assemblages,name)
     except Exception as e: print(e)
 
-    return [
-        Ts, Ps, comps
-    ]
+    return [Ts, Ps, assemblages]
+
+def plot_result(Ts,Ps,assemblages,name):
+    
+    get_hash = lambda assemblage : " ".join(sorted(assemblage.names[assemblage.amounts>0.001]))
+    comps = np.vectorize(get_hash)(assemblages)
+
+    Tmin = np.amin(Ts)
+    Tmax = np.amax(Ts)
+    Pmin = np.amin(Ps)
+    Pmax = np.amax(Ps)
+
+    categories = np.unique(comps).flatten()
+    print(categories)
+
+    colormap = np.vstack((plt.cm.tab20b(np.linspace(0., 1, 20)), plt.cm.tab20c(np.linspace(0, 1, 20))))
+
+    colorLookup = dict([(key, value) for key, value in zip(categories, colormap)])
+    print(colorLookup.keys())
+
+    colors = [colorLookup[c] for c in comps.flatten()]
+
+    shape = (comps.shape[0], comps.shape[1], 4)
+    colorgrid = np.reshape(colors, shape)
+
+    fig = plt.figure(figsize=(10,8))
+    ax1 = plt.subplot(4,8,(1,7))
+    for q,cat in enumerate(categories):
+        indx = comps.flatten() == cat
+        ax1.scatter(Ts.flatten()[indx], Ps.flatten()[indx], label=cat, color=colorLookup[cat])
+    plt.xlim([Tmin,Tmax])
+    plt.ylim([Pmin,Pmax])
+    plt.ylabel("Pressure (Pa)")
+
+    ax1.legend(loc='center left')
+
+    ax2 = plt.subplot(4,8,(1,15))
+    ax2.imshow(colorgrid, origin='lower', extent=(Tmin,Tmax,Pmin/units.GPA,Pmax/units.GPA), aspect="auto")
+    plt.ylabel("Pressure (GPa)")
+    plt.xlabel("Temperature (K)")
+
+    ax3 = plt.subplot(4,8,(4,16))
+    ax3.legend(*ax1.get_legend_handles_labels(), loc='center left')
+    ax3.axis('off')
+
+    ax4 = plt.subplot(4,8,(1,15))
+    im=ax2.imshow(n_phases, origin='lower', extent=(Tmin,Tmax,Pmin/units.GPA,Pmax/units.GPA), aspect="auto", cmap="Blues")
+    plt.ylabel("Pressure (GPa)")
+    plt.xlabel("Temperature (K)")
+    plt.colorbar(im)
+
+    fig.suptitle(name)
+    plt.savefig(name + "_"+ dt.datetime.now().isoformat() + ".png",facecolor='white', transparent=False)
             
--- systems/ec/python/tcg_slb/endmember.py
+++ systems/ec/python/tcg_slb/endmember.py
@@ -3,47 +3,33 @@ import re
 import types
 from thermocodegen.coder import coder
 
-class SLBEndmember:
+class TCGEndmember:
     name    = None
     formula = None
     model   = None
+    param_strs = None
+    param_unit = None
     param_vals = None
-    A       = None
+    A = None # Helmholtz
+    G = None # Gibbs
+    syms = None
+    param_syms = None
+    model_type = 'TP' # or 'TV'
     
-    def __init__(self,name,formula,reference,**kwargs):
+    def __init__(self,name,formula,reference,model_type):
         self.name = name
         self.formula = formula
         self.reference = reference
-        self.model = coder.StdStateModel.from_type('TV')
+        self.model_type = model_type
+
+        self.model = coder.StdStateModel.from_type(self.model_type)
+
         self.T = self.model.get_symbol_for_t()
         self.V = self.model.get_symbol_for_v()
         self.T_r = self.model.get_symbol_for_tr()
         self.V_r = self.model.get_symbol_for_vr()
-        
-        self.param_strs = [ 'a0', 'n', 'v0', \
-                            'k00', 'k0p', 'theta0', \
-                            'gamma0', 'q', 'r_Fe', \
-                            'R' ]
-        self.param_unit = [ 'J/m', '', 'J/bar-m',\
-                            'bar', '', 'K', \
-                            '', '', '', \
-                            'J/K-m' ]
-        symdict = dict( ( p, sym.symbols(p,real=True) ) for p in self.param_strs )
-        self.syms = types.SimpleNamespace(**symdict)
-
-        self.param_syms = [ symdict[p] for p in self.param_strs ]
-        
-        required_params = self.param_strs+['T_r','V_r']
-        required_params.remove('R')    # R isn't actually required
-        required_params.remove('r_Fe') # neither is r_Fe
-        
-        missing_params = [ p for p in required_params if p not in kwargs ]
-        if len(missing_params) > 0:
-            raise Exception("Not all parameter values provided.  Missing: "+", ".join(missing_params))
-        
-        self.param_vals = dict( (p, kwargs[p]) for p in required_params )
-        self.param_vals['r_Fe'] = self.r_Fe()
-        self.param_vals['R'] = kwargs.get('R', 8.31446261815324)
+        self.P = self.model.get_symbol_for_p()
+        self.P_r = self.model.get_symbol_for_pr() 
     
     def r_Fe(self):
         """Return number of Fe atoms in a formula."""
@@ -81,6 +67,18 @@ class SLBEndmember:
     
     def A_default(self):
         return self.A_iso_default() + self.A_quasi_default() + self.A_mag_default()
+    
+    
+    def G_Pr_default(self):   
+        # Heat Capacity
+        Cp_Pr = self.syms.k0+self.syms.k1/sym.sqrt(self.T)+self.syms.k2/self.T**2+self.syms.k3/self.T**3
+        return self.syms.H_TrPr + sym.integrate(Cp_Pr,(self.T,self.T_r,self.T)) - self.T*(self.syms.S_TrPr + sym.integrate(Cp_Pr/self.T,(self.T,self.T_r,self.T)))
+    
+    def G_PrToP_default(self):
+        return sym.integrate(self.syms.V_TrPr*(1+self.syms.v1*(self.P-self.P_r)+self.syms.v2*(self.P-self.P_r)**2+self.syms.v3*(self.T-self.T_r)+self.syms.v4*(self.T-self.T_r)**2),(self.P,self.P_r,self.P))
+
+    def G_default(self):
+        return self.G_Pr_default() + self.G_PrToP_default()
 
     def add_parameter(self, name, unit, symbol, value):
         self.param_strs.append(name)
@@ -101,7 +99,10 @@ class SLBEndmember:
         return values_dict
     
     def add_potential_to_model(self):
-        self.model.add_potential_to_model('A', self.A, self.params())
+        if self.model_type == 'TV':
+            self.model.add_potential_to_model('A', self.A, self.params())
+        else: # 'TP'
+            self.model.add_potential_to_model('G', self.G, self.params())
 
     def set_model_values(self):
         values_dict = self.model.get_values()
@@ -109,8 +110,151 @@ class SLBEndmember:
         self.model.set_values(values_dict)
     
     def tofile(self,outdir):
-        if self.A is None: self.A = self.A_default()
+        if self.model_type == 'TV':
+            if self.A is None: self.A = self.A_default()
+        else:
+            if self.G is None: self.G = self.G_default()
+
         self.add_potential_to_model()
         self.set_model_values()
         filename = self.model.to_xml(path=outdir)
         return filename
+
+
+class SLBEndmember(TCGEndmember):
+
+    def __init__(self,name,formula,reference,**kwargs):
+
+        super().__init__(name,formula,reference,'TV')
+
+        self.param_strs = [ 'a0', 'n', 'v0', \
+                            'k00', 'k0p', 'theta0', \
+                            'gamma0', 'q', 'r_Fe', \
+                            'R' ]
+        self.param_unit = [ 'J/m', '', 'J/bar-m',\
+                            'bar', '', 'K', \
+                            '', '', '', \
+                            'J/K-m' ]
+        symdict = dict( ( p, sym.symbols(p,real=True) ) for p in self.param_strs )
+        self.syms = types.SimpleNamespace(**symdict)
+
+        self.param_syms = [ symdict[p] for p in self.param_strs ]
+        
+        required_params = self.param_strs+['T_r','V_r']
+        required_params.remove('R')    # R isn't actually required
+        required_params.remove('r_Fe') # neither is r_Fe
+        
+        missing_params = [ p for p in required_params if p not in kwargs ]
+        if len(missing_params) > 0:
+            raise Exception("Not all parameter values provided.  Missing: "+", ".join(missing_params))
+        
+        self.param_vals = dict( (p, kwargs[p]) for p in required_params )
+        self.param_vals['r_Fe'] = self.r_Fe()
+        self.param_vals['R'] = kwargs.get('R', 8.31446261815324)
+
+
+class BermanEndmember(TCGEndmember):
+    
+    def __init__(self,name,formula,reference,**kwargs):
+        super().__init__(name,formula,reference,'TP')
+
+        self.param_strs = [ 'H_TrPr', 'S_TrPr', 'V_TrPr', \
+                            'k0', 'k1', 'k2', 'k3',\
+                            'v1', 'v2', 'v3', 'v4']
+
+        self.param_unit = [ 'J', 'J/K', 'J/bar',\
+                            'J/K-m', 'J/K^(1/2)-m', 'J-K/m', 'J-K^2',\
+                            '1/bar', '1/bar^2', '1/K', '1/K^2']
+        symdict = dict( ( p, sym.symbols(p,real=True) ) for p in self.param_strs )
+        self.syms = types.SimpleNamespace(**symdict)
+
+        self.param_syms = [ symdict[p] for p in self.param_strs ]
+        
+        required_params = self.param_strs
+        
+        missing_params = [ p for p in required_params if p not in kwargs ]
+        if len(missing_params) > 0:
+            raise Exception("Not all parameter values provided.  Missing: "+", ".join(missing_params))
+        
+        self.param_vals = dict( (p, kwargs[p]) for p in required_params )
+        self.param_vals['r_Fe'] = self.r_Fe()
+
+class BermanPolyEndmember(TCGEndmember):
+    
+    # instead of a row of a datafram as a dict (**kwargs),
+    # accept a dataframe consisting of multiple rows
+    def __init__(self,name,formula,reference,polymorphs,df):
+
+        # init the SS model
+        super().__init__(name,formula,reference,'TP')      
+     
+     
+        abbrevs = df['sAbbrev'].tolist()
+        # concatenate the param strings from all rows using abbreviations
+        # concatenate the needed units as well (they are the same for each row)
+        root_strs = [
+            'H_TrPr', 'S_TrPr', 'V_TrPr',
+            'k0', 'k1', 'k2', 'k3',
+            'v1', 'v2', 'v3', 'v4'
+        ]
+        root_unit = [
+            'J', 'J/K', 'J/bar',
+            'J/K-m', 'J/K^(1/2)-m', 'J-K/m', 'J-K^2',
+            '1/bar', '1/bar^2', '1/K', '1/K^2'
+        ]
+        param_strs = []
+        param_unit = []
+
+        params_root = coder.set_coder_params(root_strs, root_unit)
+        symbol_dict_root = coder.get_symbol_dict_from_params(params_root)
+
+        for index, row in df.iterrows():
+            abbrev = row['sAbbrev']
+            param_strs += ['{}_{}'.format(p,abbrev) for p in root_strs]
+            param_unit += root_unit
+
+     
+        self.param_strs = param_strs
+        self.param_unit = param_unit
+
+        # update coder
+        params = coder.set_coder_params(self.param_strs, self.param_unit)
+        symbol_dict = coder.get_symbol_dict_from_params(params)
+
+        self.param_syms = [ symbol_dict[p] for p in self.param_strs ]
+        self.syms = types.SimpleNamespace(**symbol_dict_root, **symbol_dict)
+
+        # override P,T, etc. with updated coder model
+        self.model = coder.StdStateModel.from_type()
+        self.T = self.model.get_symbol_for_t()
+        self.P = self.model.get_symbol_for_p()
+        self.T_r = self.model.get_symbol_for_tr()
+        self.P_r = self.model.get_symbol_for_pr()
+
+        self.V = self.syms.V_TrPr*(1+self.syms.v1*(self.P-self.P_r)+self.syms.v2*(self.P-self.P_r)**2+self.syms.v3*(self.T-self.T_r)+self.syms.v4*(self.T-self.T_r)**2)
+        
+        # override self.G for solid solution
+        GPrToP = sym.integrate(self.V,(self.P,self.P_r,self.P))
+        GPr = self.G_Pr_default()
+        G_ss = GPr + GPrToP
+        
+        N = len(root_strs)
+        G_p = [sym.Symbol('G_{}'.format(a)) for a in abbrevs]
+        symbol_list = list(symbol_dict.values())
+
+        for i,p in enumerate(polymorphs):
+            subs_dict = dict(zip(symbol_dict_root.values(),symbol_list[i*N:(i+1)*N]))
+            G_p[i]=G_ss.subs(subs_dict)
+        
+        self.G = sym.Min(*G_p)
+        
+        param_vals = dict()
+        for ind, row in df.iterrows():
+            abbrev = row.pop('sAbbrev')
+            new_columns = ['{}_{}'.format(p,abbrev) for p in root_strs]
+            row.rename(index=dict(zip(root_strs,new_columns)),inplace=True)
+            param_vals.update(row.to_dict())
+
+        self.param_vals = param_vals
+        self.param_vals['r_Fe'] = self.r_Fe()
+        self.param_vals['R'] = 8.31446261815324
